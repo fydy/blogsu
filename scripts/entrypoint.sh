d18f08789a090be0eb273cc41806294c29185489
iamwwc <iam.wuweichao@gmail.com>
2019-09-10 23:09:47 +0800

    done

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+base="/app/src/config/"
+mkdir -p $base
 if [[ $# -ne 0 ]]; then
   url=$1
-  curl -s -L $url > /app/src/config/index.js
+  curl -s -L $url > "${base}index.js"
 fi
 
 node scripts/prod-server.js
